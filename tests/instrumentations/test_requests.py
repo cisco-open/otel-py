94c1a2cbb63953885476a557b3e67ca95e3ea26b
Sami Musallam <samusall@cisco.com>
2022-03-28 15:43:01 +0300

    feat(add-http-request-body): refactored test_requests.py + added requests body test

--- tests/instrumentations/test_requests.py
+++ tests/instrumentations/test_requests.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import json
 import unittest
 
 from opentelemetry.sdk.trace import ReadableSpan
@@ -23,31 +24,42 @@ from cisco_otel_py import tracing, options, consts
 from tests import utils
 from cisco_opentelemetry_specifications import SemanticAttributes
 
-from requests import get
+from requests import get, post
 
 
 class TestRequests(unittest.TestCase):
-    async def test_http_request_headers(self):
+    provider = None
+    exporter = InMemorySpanExporter()
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.provider = tracing.init(
+            cisco_token=utils.TEST_TOKEN,
+            exporters=[
+                options.ExporterOptions(
+                    exporter_type=consts.TEST_EXPORTER_TYPE,
+                    collector_endpoint=utils.LOCAL_COLLECTOR,
+                )
+            ],
+        )
+
+        cls.provider.add_span_processor(SimpleSpanProcessor(cls.exporter))
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.provider = None
+
+    def tearDown(self) -> None:
+        self.exporter.clear()
+
+    def test_http_request_headers(self):
         try:
-            provider = tracing.init(
-                cisco_token=utils.TEST_TOKEN,
-                exporters=[
-                    options.ExporterOptions(
-                        exporter_type=consts.TEST_EXPORTER_TYPE,
-                        collector_endpoint=utils.LOCAL_COLLECTOR,
-                    )
-                ],
-            )
-
-            exporter = InMemorySpanExporter()
-            provider.add_span_processor(SimpleSpanProcessor(exporter))
-
             get(
                 url="https://google.com/",
                 headers={"test-header-key": "test-header-value"},
             )
 
-            spans = exporter.get_finished_spans()
+            spans = self.exporter.get_finished_spans()
 
             self.assertEqual(len(spans), 1)
 
@@ -59,7 +71,31 @@ class TestRequests(unittest.TestCase):
                 ],
                 "test-header-value",
             )
+        except Exception as err:
+            self.fail(f"unexpected exception: {err}")
 
+    def test_http_request_body(self):
+        try:
+            post("https://google.com/", json={"test-key": "test-value"})
+
+            spans = self.exporter.get_finished_spans()
+
+            self.assertEqual(len(spans), 1)
+
+            span: ReadableSpan = spans[0]
+
+            self.assertIn(
+                f"{SemanticAttributes.HTTP_REQUEST_BODY.key}", span.attributes
+            )
+
+            request_body = json.loads(
+                span.attributes[f"{SemanticAttributes.HTTP_REQUEST_BODY.key}"]
+            )
+
+            self.assertEqual(
+                request_body["test-key"],
+                "test-value",
+            )
         except Exception as err:
             self.fail(f"unexpected exception: {err}")
 

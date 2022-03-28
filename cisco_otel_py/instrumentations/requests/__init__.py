3ababbfc2c0e2ab59d576ef83ae7326c697f0d0e
Sami Musallam <samusall@cisco.com>
2022-03-28 11:11:11 +0300

    feat(add-http-request-body): removed unused code

--- cisco_otel_py/instrumentations/requests/__init__.py
+++ cisco_otel_py/instrumentations/requests/__init__.py
@@ -4,11 +4,6 @@ from cisco_otel_py.instrumentations import BaseInstrumentorWrapper
 
 def get_active_span_for_call_wrapper(requests_wrapper):
     def get_active_span_for_call(span, response) -> None:
-        response_content = None
-        if hasattr(response, "content"):
-            response_content = response.content.decode()
-        else:
-            response_content = ""
         request_content = None
         if hasattr(response.request, "body"):
             request_content = response.request.body.decode()

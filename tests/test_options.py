8a0f8cad1101cb61272850a952275dcc178fd788
Osher Vaknin <osherv@cisco.com>
2022-04-06 11:54:14 +0300

    fix(tests): refactored options test

--- tests/test_options.py
+++ tests/test_options.py
@@ -13,9 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
+import os
 import unittest
-from os import environ
+from unittest import mock
 
 from cisco_otel_py import options, consts
 
@@ -23,19 +23,10 @@ from . import utils
 
 
 class TestOptions(unittest.TestCase):
-    def setUp(self) -> None:
-        utils.clean_env_vars(
-            [
-                consts.KEY_SERVICE_NAME,
-                consts.KEY_EXPORTER_TYPE,
-                consts.KEY_COLLECTOR_ENDPOINT,
-            ]
-        )
-
     def test_defaults(self):
         opt = options.Options(cisco_token=utils.TEST_TOKEN)
 
-        self.assertEqual(environ.get(consts.KEY_SERVICE_NAME), consts.DEFAULT_SERVICE_NAME)
+        self.assertEqual(opt.service_name, consts.DEFAULT_SERVICE_NAME)
         self.assertEqual(opt.max_payload_size, consts.MAX_PAYLOAD_SIZE)
 
     def test_parameters(self):
@@ -49,26 +40,44 @@ class TestOptions(unittest.TestCase):
         self.assertEqual(opt.service_name, "Service")
         self.assertEqual(opt.max_payload_size, 1023)
 
+    @mock.patch.dict(
+        os.environ,
+        {
+            consts.KEY_SERVICE_NAME: "Service",
+            consts.KEY_TOKEN: utils.TEST_TOKEN
+
+        }
+    )
+    def test_parameters_from_env(self):
+        opt = options.Options()
+
+        self.assertEqual(opt.cisco_token, utils.TEST_TOKEN)
+        self.assertEqual(opt.service_name, "Service")
+
     def test_token_is_missing(self):
         with self.assertRaisesRegex(ValueError, "Can not initiate cisco-otel launcher without token"):
             _ = options.Options()
 
 
 class TestExporterOptions(unittest.TestCase):
-    def setUp(self) -> None:
-        utils.clean_env_vars(
-            [
-                consts.KEY_SERVICE_NAME,
-                consts.KEY_EXPORTER_TYPE,
-                consts.KEY_COLLECTOR_ENDPOINT,
-            ]
-        )
-
     def test_defaults(self):
         exporter_opts = options.ExporterOptions()
 
         self.assertEqual(exporter_opts.exporter_type, consts.DEFAULT_EXPORTER_TYPE)
         self.assertEqual(exporter_opts.collector_endpoint, consts.DEFAULT_COLLECTOR_ENDPOINT)
+
+    @mock.patch.dict(
+        os.environ,
+        {
+            consts.KEY_COLLECTOR_ENDPOINT: "env_endpoint",
+            consts.KEY_EXPORTER_TYPE: consts.HTTP_EXPORTER_TYPE
+        }
+    )
+    def test_parameters_from_env(self):
+        exporter_opts = options.ExporterOptions()
+
+        self.assertEqual(exporter_opts.exporter_type, consts.HTTP_EXPORTER_TYPE)
+        self.assertEqual(exporter_opts.collector_endpoint, 'env_endpoint')
 
     def test_parameters(self):
         exporter_opts = options.ExporterOptions(consts.HTTP_EXPORTER_TYPE, 'collector_endpoint')

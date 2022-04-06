35ccdaf0c1ecae7eaba860d564ebe6835e3baf36
Osher Vaknin <osherv@cisco.com>
2022-04-06 14:46:35 +0300

    fix(tests): added tracing tests

--- tests/test_exporter_factory.py
+++ tests/test_exporter_factory.py
@@ -1,3 +1,30 @@
 import unittest
 
-class TestExporterFactory
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter as OTLPGrpcExporter,
+)
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+    OTLPSpanExporter as OTLPHTTPExporter,
+)
+
+from cisco_otel_py import consts, options, exporter_factory
+
+
+class TestExporterFactory(unittest.TestCase):
+    def setUpClass(cls) -> None:
+        cls.test_options = options.Options(cisco_token='sometoken')
+
+    def test_otlp_exporter(self):
+        self.test_options.exporters = [options.ExporterOptions(
+            exporter_type=consts.GRPC_EXPORTER_TYPE,
+            collector_endpoint='my-end'
+        )]
+
+        exporters = exporter_factory.init_exporters(self.test_options)
+        self.assertEqual(len(exporters), 1)
+
+        otlp_exporter = exporters[0]
+        self.assertIsInstance(otlp_exporter, OTLPGrpcExporter)
+        import ipdb;ipdb.set_trace()
+

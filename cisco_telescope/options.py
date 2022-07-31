faef3ae01ddeea050e04cc78c078576424d26889
Nimrod Shlagman <nemos@cisco.com>
2022-07-31 12:28:54 +0300

    feat(easy-mig): cr fix2

--- cisco_telescope/options.py
+++ cisco_telescope/options.py
@@ -80,8 +80,6 @@ class Options:
         exporters: [ExporterOptions] = None,
     ):
 
-        os.environ.setdefault("PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION", "python")
-
         # Set options parameters
         self.service_name = service_name or os.environ.get(
             Consts.SERVICE_NAME_KEY, Consts.DEFAULT_SERVICE_NAME
